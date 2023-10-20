96507f99d55f37f5665f3ef21bd92affe4b14cb8
Maximiliano D'Amelio <maximilianodamelio@gmail.com>
2023-10-20 16:53:57 -0300

    header added + cleaned scaffold

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,12 +1,14 @@
 <template>
   <v-app>
-    <default-bar />
-
+    <Header></Header>
+    <!-- <default-bar /> -->
+    <sidebar></sidebar>
     <default-view />
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  import DefaultBar from './AppBar.vue'
-  import DefaultView from './View.vue'
+import DefaultView from "./View.vue";
+import Header from "./Header.vue";
+import Sidebar from "./Sidebar.vue";
 </script>
---
+++ src/layouts/default/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app-bar flat dark color="primary" height="63">
+    <v-app-bar-title>
+      <v-img width="146" src="@/assets/edmachinaLogo.svg" />
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-web</v-icon>
+    </v-btn>
+
+    <v-btn icon="mdi-moon-waning-crescent"> </v-btn>
+
+    <v-btn icon="mdi-bell">
+      <v-badge :content="user.notifications" color="error">
+        <v-icon>mdi-bell</v-icon>
+      </v-badge>
+    </v-btn>
+    <template v-slot:append>
+      <v-list-item
+        class="text-white"
+        :title="user.name"
+        :subtitle="user.role"
+      ></v-list-item>
+      <v-avatar size="36px">
+        <v-img
+          alt="Avatar"
+          src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+        ></v-img>
+      </v-avatar>
+    </template>
+  </v-app-bar>
+</template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      user: {
+        name: "Hellen Smith",
+        role: "Admin",
+        notifications: 4,
+        imgPath: "",
+      },
+    };
+  },
+};
+</script>
